1e6914ccdaff9d70a3c48fd38c4ef9ca90f70b9f
MelissaGarrett <melissa_garrett@hotmail.com>
2018-11-12 15:07:25 -0600

    Initial Commit

---
+++ CheckoutTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CheckoutTime
+//
+//  Created by Melissa  Garrett on 11/12/18.
+//  Copyright © 2018 MelissaGarrett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
