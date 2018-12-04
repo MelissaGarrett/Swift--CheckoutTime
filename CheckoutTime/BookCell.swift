094c75fab7b191a8646b7fe0250739a7930f1e0a
MelissaGarrett <melissa_garrett@hotmail.com>
2018-12-03 23:45:00 -0600

    First commit

---
+++ CheckoutTime/BookCell.swift
@@ -0,0 +1,24 @@
+//
+//  BookCell.swift
+//  CheckoutTime
+//
+//  Created by Melissa  Garrett on 11/12/18.
+//  Copyright © 2018 MelissaGarrett. All rights reserved.
+//
+
+import UIKit
+
+class BookCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ CheckoutTime/Genre.swift
@@ -0,0 +1,9 @@
+//
+//  Genre.swift
+//  CheckoutTime
+//
+//  Created by Melissa  Garrett on 11/12/18.
+//  Copyright © 2018 MelissaGarrett. All rights reserved.
+//
+
+import Foundation
